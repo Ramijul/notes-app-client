fcb835543345995bdbdbb36e8c5371f273caf51f
Rami <ramizul127@hotmail.com>
2022-01-12 16:12:06 -0500

    added signup flow

--- src/PageRoutes.js
+++ src/PageRoutes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 import NotFound from "./containers/NotFound";
 
 
@@ -11,6 +12,7 @@ export default function PageRoutes() {
             <Route path='*' element={<NotFound />} />
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
         </Routes>
     );
 }
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -6,18 +6,21 @@ import { onError } from "../libs/errorLib";
 import { Auth } from "aws-amplify";
 import { useAppContext } from "../libs/contextLib";
 import { useNavigate } from "react-router-dom";
-
+import { useFormFields } from "../libs/hooksLib";
 
 
 export default function Login() {
+    const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(false);
     const { userHasAuthenticated } = useAppContext();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
+    const [fields, handleFieldChange] = useFormFields({
+        email: "",
+        password: ""
+    });
+
 
     function validateForm() {
-        return email.length > 0 && password.length > 0;
+        return fields.email.length > 0 && fields.password.length > 0;
     }
 
     async function handleSubmit(event) {
@@ -25,7 +28,7 @@ export default function Login() {
         setIsLoading(true);
 
         try {
-            await Auth.signIn(email, password);
+            await Auth.signIn(fields.email, fields.password);
             userHasAuthenticated(true);
             navigate("/");
         } catch (e) {
@@ -42,16 +45,16 @@ export default function Login() {
                     <Form.Control
                         autoFocus
                         type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
+                        value={fields.email}
+                        onChange={handleFieldChange}
                     />
                 </Form.Group>
                 <Form.Group size="lg" controlId="password">
                     <Form.Label>Password</Form.Label>
                     <Form.Control
                         type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                        value={fields.password}
+                        onChange={handleFieldChange}
                     />
                 </Form.Group>
 
